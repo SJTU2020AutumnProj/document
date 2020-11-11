dc6251a70db0e73c7250d1057f79e8222bafcf5f
shenhliu <55398344+shenhliu@users.noreply.github.com>
2020-11-11 21:31:56 +0800

    doc:更新目前用的sql
    因为go中course是保留字，我把sql里的course改成courseclass了

---
+++ Second iteration/数据库/11.11数据库.sql
@@ -0,0 +1,46 @@
+drop table if exists `courseclass`;
+create table `courseclass`
+(
+   course_id            int not null auto_increment,
+   course_name          varchar(100) not null,
+   introduction         varchar(1000),
+   textbooks            varchar(1000),
+   start_time           time,
+   end_time             time,
+   primary key (course_id)
+);
+
+drop table if exists `user`;
+create table `user`
+(
+   user_id                 int not null auto_increment,
+   user_type               int not null default 1,
+   username                varchar(100) not null,
+   password                varchar(100) not null,
+   school                  varchar(100) not null,
+   ID                      varchar(100) not null,
+   phone                   varchar(100),
+   email                   varchar(100),
+   primary key (user_id)
+);
+
+drop table if exists `take`;
+create table `take`
+(
+   `user_id`                 int not null,
+   `course_id`            int not null ,
+   primary key (`user_id`, `course_id`),
+   foreign key (`user_id`) REFERENCES `user` (`user_id`),
+   foreign key (`course_id`) REFERENCES `course` (`course_id`)
+);
+
+drop table if exists `teach`;
+create table `teach`
+(
+   `user_id`                 int not null,
+   `course_id`            int not null ,
+   `role`                 varchar(30) ,
+   primary key (`user_id`, `course_id`),
+   foreign key (`user_id`) REFERENCES `user` (`user_id`),
+   foreign key (`course_id`) REFERENCES `course` (`course_id`)
+);
