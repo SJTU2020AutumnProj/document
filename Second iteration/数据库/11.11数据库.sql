14edfe73820cbd3ba24f4c7cbb90c1948d3d374f
shenhliu <55398344+shenhliu@users.noreply.github.com>
2020-11-12 20:56:03 +0800

    Update 11.11数据库.sql

--- Second iteration/数据库/11.11数据库.sql
+++ Second iteration/数据库/11.11数据库.sql
@@ -15,7 +15,7 @@ create table `user`
 (
    user_id                 int not null auto_increment,
    user_type               int not null default 1,
-   username                varchar(100) not null,
+   user_name                varchar(100) not null,
    password                varchar(100) not null,
    school                  varchar(100) not null,
    ID                      varchar(100) not null,
