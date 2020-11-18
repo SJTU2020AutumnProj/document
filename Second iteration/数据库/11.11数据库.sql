88f58511b7f6d883c8501cd3fbe01a133d2516d6
shenhliu <55398344+shenhliu@users.noreply.github.com>
2020-11-18 13:30:25 +0800

    fix：修改了start_time end_time类型

--- Second iteration/数据库/11.11数据库.sql
+++ Second iteration/数据库/11.11数据库.sql
@@ -5,8 +5,8 @@ create table `courseclass`
    course_name          varchar(100) not null,
    introduction         varchar(1000),
    text_books            varchar(1000),
-   start_time           time,
-   end_time             time,
+   start_time           date,
+   end_time             date,
    primary key (course_id)
 );
 
