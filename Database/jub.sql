1e678d5a6db5ab99ce6b16a05046e62fc4b24991
shenhliu <55398344+shenhliu@users.noreply.github.com>
2020-12-22 18:36:10 +0800

    更新数据库，为Homework插入初始数据

--- Database/jub.sql
+++ Database/jub.sql
@@ -54,3 +54,8 @@ db.createCollection("homework");
 // ----------------------------
 // Documents of homework
 // ----------------------------
+db.getCollection("homework").insert([ {
+    _id: ObjectId("5fd8916f6506000015006573"),
+    "homework_id": 1,
+    "homework_json": "47BCE5C74F589F4867DBD57E9CA9F808"
+} ]);
